a442849b108cfa234b585cbac755c9c6f7e13efd
Colin <colin1124x@gmail.com>
2014-08-20 10:09:11 +0800

    調整route

--- app/routes.php
+++ app/routes.php
@@ -5,4 +5,10 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('023af8434a106f0d86a42e576a23ac70', 'DevTestController@index');
+Route::get(
+    '023af8434a106f0d86a42e576a23ac70',
+    array(
+        'as' => 'dev',
+        'uses' => 'DevTestController@index',
+    )
+);
